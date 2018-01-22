e35cf944e6a19bdf3ed3e0df5458571ae609dfb3
Mac User <macuser@Macs-MacBook-Pro.local>
2018-01-22 12:28:55 -0800

    begin css transition

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './index.css';
-
+// import './index.css';
+import './movie-style.css';
 // if this follows/replaces my org script.jsx, ref below
 // added from my App
 // import Fetch from './Fetch/Fetch.jsx';
