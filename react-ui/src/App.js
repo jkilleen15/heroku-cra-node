917acc33a80068bdcfea24a9abbe70d826d8a3d9
Mac User <macuser@Macs-MacBook-Pro.local>
2018-01-22 12:32:07 -0800

    part 2 css transition

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 // added from my app
 // import Fetch from './Fetch/Fetch.jsx';
